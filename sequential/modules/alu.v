70bace36c7abfacb23cf6abe469932d28673bea3
wig-nesh <vigneshvembar.m@students.iiit.ac.in>
2025-02-18 16:47:09 +0530

    Made ALU modular

--- sequential/modules/alu.v
+++ sequential/modules/alu.v
@@ -2,8 +2,13 @@
 
 `include "full_adder.v"
 `include "mux2to1.v"
+`include "add_sub.v"
+`include "xor.v"
+`include "or.v"
+`include "and.v"
 `include "sll.v"
 `include "sr.v"
+`include "slt_sltu.v"
 
 module alu(
     input [31:0] instruction,
@@ -14,59 +19,20 @@ module alu(
 
     wire [2:0] funct3 = instruction[14:12];
     wire [6:0] funct7 = instruction[31:25];
-    genvar i;
 
     // ----------------ADD/SUB----------------
 
     wire [63:0] sum_out;
     wire [64:0] carry;
 
-    // set to sub if instruction sub, or slt/sltu
-    // this couldve been one line with operators :(
-    wire temp1a;
-    wire temp1b;
-    wire temp2a;
-    wire temp2b;
-    wire temp3a;
-    wire temp3b;
-    wire temp3c;
-    wire temp4;
-    wire sub;
-    and (temp1a, funct3[0], funct3[1]);
-    and (temp1b, temp1a, ~funct3[2]);
-    and (temp2a, ~funct3[0], funct3[1]);
-    and (temp2b, temp2a, ~funct3[2]);
-    and (temp3a, ~funct3[0], ~funct3[1]);
-    and (temp3b, temp3a, ~funct3[2]);
-    and (temp3c, temp3b, funct7[5]);
-    or (temp4, temp1b, temp2b);
-    or (sub, temp4, temp3c);
-
-    // xor in2 with 0xFFFFFFFF if sub to invert
-    wire [63:0] in2_mod;
-    generate
-        for (i = 0; i < 64; i = i + 1) begin : xor_sub
-            xor(
-                in2_mod[i],
-                in2[i],
-                sub
-            );
-        end
-    endgenerate
-
-    assign carry[0] = sub; // add 1 for subtraction
-
-    generate
-        for (i = 0; i < 64; i = i + 1) begin : adder_chain
-            full_adder fa(
-                .a(in1[i]),
-                .b(in2_mod[i]),
-                .cin(carry[i]),
-                .sum(sum_out[i]),
-                .cout(carry[i+1])
-            );
-        end
-    endgenerate
+    add_sub add_sub_unit(
+        .in1(in1),
+        .in2(in2),
+        .funct7(funct7),
+        .funct3(funct3),
+        .sum_out(sum_out),
+        .carry(carry)
+    );
 
     // ----------------XOR/OR/AND----------------
 
@@ -75,25 +41,21 @@ module alu(
     wire [63:0] and_out;
 
     // literally just chained gates
-    generate
-        for (i = 0; i < 64; i = i + 1) begin : xor_or_and
-            xor(
-                xor_out[i],
-                in1[i],
-                in2[i]
-            );
-            or(
-                or_out[i],
-                in1[i],
-                in2[i]
-            );
-            and(
-                and_out[i],
-                in1[i],
-                in2[i]
-            );
-        end
-    endgenerate
+    xor_ xor_unit(
+        .in1(in1),
+        .in2(in2),
+        .out(xor_out)
+    );
+    or_ or_unit(
+        .in1(in1),
+        .in2(in2),
+        .out(or_out)
+    );
+    and_ and_unit(
+        .in1(in1),
+        .in2(in2),
+        .out(and_out)
+    );
 
     // ----------------SLL----------------
 
@@ -123,10 +85,11 @@ module alu(
     wire slt_out;
     wire sltu_out;
 
-    assign slt_out = sum_out[63];
-    not(
-        sltu_out,
-        carry[64]
+    slt_sltu slt_sltu_unit(
+        .sum_out(sum_out),
+        .carry(carry),
+        .slt_out(slt_out),
+        .sltu_out(sltu_out)
     );
 
     always @(*) begin
---
+++ sequential/modules/slt_sltu.v
@@ -0,0 +1,11 @@
+module slt_sltu(
+    input [63:0] sum_out,
+    input [63:0] carry,
+    output slt_out,
+    output sltu_out
+);
+    assign slt_out = sum_out[63];
+    not(
+        sltu_out,
+        carry[64]
+);

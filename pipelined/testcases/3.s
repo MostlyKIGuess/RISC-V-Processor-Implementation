f0bbbe55cdbe5946af2241c485206c9f2ec7fb1d
wig-nesh <vigneshvembar.m@students.iiit.ac.in>
2025-03-08 19:12:45 +0530

    everything works except 2.s

--- pipelined/testcases/3.s
+++ pipelined/testcases/3.s
@@ -1,6 +1,8 @@
 begin:
     beq x0, x0, L1
     addi x6, x0, 4
+    addi x6, x0, 4
+    addi x6, x0, 4
 
 L1:
     addi x5, x0, 4
--- pipelined/testcases/4.s
+++ pipelined/testcases/4.s
@@ -1,7 +1,5 @@
 begin:
     addi x4, x0, 1
-    addi x8, x0, 1
-    addi x8, x0, 1
     beq x0, x4, L1
     addi x6, x0, 4
 
